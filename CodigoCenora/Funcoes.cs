fb1f361245d8bed404d7e2754cb8c9ba1d25b9aa
Cenora <cenora2006@gmail.com>
2023-02-10 19:32:34 -0300

    Initial commit

---
+++ CodigoCenora/Funcoes.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoCenora
+{
+    public class Util
+    {
+        /// <summary>
+        /// Informe a cor a ser desenhada como parâmetro ao método
+        /// A cor padrão é Branco.
+        /// </summary>
+        /// <param name="cor">Nome da Cor</param>
+        public static void DesenhaCaixa(Cores cor=Cores.Branco)
+        {
+            switch (cor)
+            {
+                case Cores.Azul:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("██");
+                    break;
+                case Cores.Vermelho:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("██");
+                    break;
+                case Cores.Branco:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("██");
+                    break;
+                case Cores.Verde:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("██");
+                    break;
+                case Cores.Amarelo:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("██");
+                    break;
+                case Cores.Preto:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write("██");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Recebe uma lista de cores, com no mínimo 1 cor 
+        /// </summary>
+        /// <param name="cores"></param>
+        public static void DesenhaCaixa(List<Siglas> cores)
+        {
+            if (cores.Count >= 1)
+            {
+                foreach (var cor in cores)
+                {
+                    switch (cor)
+                    {
+                        case Siglas.b:
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("██");
+                            break;
+                        case Siglas.r:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("██");
+                            break;
+                        case Siglas.w:
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write("██");
+                            break;
+                        case Siglas.g:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("██");
+                            break;
+                        case Siglas.y:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("██");
+                            break;
+                        case Siglas.k:
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write("██");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    public enum Cores
+    {
+        Azul,
+        Vermelho,
+        Branco,
+        Verde,
+        Amarelo,
+        Preto
+    }
+
+    public enum Siglas
+    {
+        b,
+        r,
+        w,
+        g,
+        y,
+        k
+    }
+}
---
+++ DotNetFun/Program.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * https://learn.microsoft.com/en-us/dotnet/api/system.console.clear?view=net-5.0
+ * @author Cenora
+ */
+
+using CodigoCenora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.DesenhaCaixa(Cores.Azul);
+            Util.DesenhaCaixa(Cores.Branco);
+            Util.DesenhaCaixa(Cores.Vermelho);
+            Console.WriteLine();
+            Util.DesenhaCaixa(Cores.Azul);
+            Util.DesenhaCaixa(Cores.Branco);
+            Util.DesenhaCaixa(Cores.Vermelho);
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Util.DesenhaCaixa(new List<Siglas>{Siglas.b, Siglas.b, Siglas.b });
+            Console.ReadKey();
+        }
+
+
+    }
+
+    
+}
